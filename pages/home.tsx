a9d3155f98b22fb90cb3e7dd556ac9a2da2ad027
Justin Knueppel <justinknueppel@gmail.com>
2021-05-21 17:31:30 -0400

    Start with home page and delete boilerplate

---
+++ pages/home.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Home / Twitter</title>
+        <meta name="description" content="Clone of Twitter" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Hello World!</main>
+    </div>
+  );
+};
+
+export default Home;
